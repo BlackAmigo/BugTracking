5791196dfa24517dae3f1bef53396bcc0b6d4368
BlackAmigo <anatoliy.mir.pro@gmail.com>
2020-03-09 23:54:27 +0700

    edited Project.java

--- src/main/java/org/example/entities/Project.java
+++ src/main/java/org/example/entities/Project.java
@@ -1,10 +1,9 @@
 package org.example.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 public class Project {
@@ -12,10 +11,18 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @NotBlank(message = "Not blank value is required")
     private String name;
+    @NotBlank(message = "Not blank value is required")
     private String textDescription;
-    private Date createdDate;
+    private final Date createdDate;
     private Date lastModifiedDate;
+
+    public Project() {
+        Date date = new Date();
+        createdDate = date;
+        lastModifiedDate = date;
+    }
 
     public long getId() {
         return id;
@@ -45,15 +52,28 @@ public class Project {
         return createdDate;
     }
 
-    public void setCreatedDate(Date createDate) {
-        this.createdDate = createDate;
-    }
-
     public Date getLastModifiedDate() {
         return lastModifiedDate;
     }
 
     public void setLastModifiedDate(Date lastModifiedDate) {
         this.lastModifiedDate = lastModifiedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Project project = (Project) o;
+        return id == project.id &&
+                name.equals(project.name) &&
+                Objects.equals(textDescription, project.textDescription) &&
+                createdDate.equals(project.createdDate) &&
+                lastModifiedDate.equals(project.lastModifiedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, textDescription, createdDate, lastModifiedDate);
     }
 }
