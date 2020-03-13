e0741840b8bc924f1d6ba355f497cd227ab97bf2
BlackAmigo <anatoliy.mir.pro@gmail.com>
2020-03-13 23:18:30 +0700

    edited TaskService

--- src/main/java/org/example/services/TaskService.java
+++ src/main/java/org/example/services/TaskService.java
@@ -1,18 +1,11 @@
-//package org.example.services;
-//
-//import org.example.entities.Project;
-//import org.example.entities.Task;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public interface TaskService {
-//
-//    Task save(Task task);
-//    List<Task> getAll();
-//    Optional<Task> get(Long id);
-//    Task update(Task source, Task target);
-//    void delete(Task task);
-//}
+package org.example.services;
+
+import org.example.entities.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public interface TaskService extends EntityService<Task>{
+    Optional<Task> findByName(String name);
+}
--- src/main/java/org/example/services/TaskServiceImpl.java
+++ src/main/java/org/example/services/TaskServiceImpl.java
@@ -1,47 +1,59 @@
-//package org.example.services;
-//
-//import org.example.entities.Project;
-//import org.example.entities.Task;
-//import org.example.repositories.TaskRepository;
-//import org.example.utils.Utils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class TaskServiceImpl implements TaskService {
-//
-//    @Autowired
-//    private TaskRepository repository;
-//
-//    @Override
-//    public Task save(Task task) {
-//        return repository.save(task);
-//    }
-//
-//    @Override
-//    public List<Task> getAll() {
-//        return repository.findAll();
-//    }
-//
-//    @Override
-//    public Optional<Task> get(Long id) {
-//        return repository.findById(id);
-//    }
-//
-//    @Override
-//    public Task update(Task source, Task target) {
-//            Optional<Task> project = repository.findById(source.getId());
-//            if (project.isPresent()) {
-//                Utils.copyNotNullProperties(source, target);
-//                return target;
-//            } else return null;
-//    }
-//
-//    @Override
-//    public void delete(Task task) {
-//        repository.delete(task);
-//    }
-//}
+package org.example.services;
+
+import org.example.entities.Task;
+import org.example.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
+
+import static org.example.utils.Utils.copyNotNullProperties;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    @Autowired
+    private TaskRepository repository;
+
+    @Override
+    public Task save(Task task) {
+        task.setLastModifiedDate(new Date());
+        return repository.save(task);
+    }
+
+    @Override
+    public Task update(Task source, Task target) {
+        if (target != null && source != null) {
+            copyNotNullProperties(source, target);
+            return save(target);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Page<Task> getAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Task> getById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public boolean delete(Task task) {
+        if (task != null && getById(task.getId()).isPresent()) {
+            repository.delete(task);
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public Optional<Task> findByName(String name) {
+        return repository.findByName(name);
+    }
+}
