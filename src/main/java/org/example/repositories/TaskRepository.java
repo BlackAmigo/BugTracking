7699f3daff9748eba628c9570c4098ee0fef2dce
BlackAmigo <anatoliy.mir.pro@gmail.com>
2020-03-13 23:09:49 +0700

    edited TaskRepository

--- src/main/java/org/example/repositories/TaskRepository.java
+++ src/main/java/org/example/repositories/TaskRepository.java
@@ -1,9 +1,12 @@
 package org.example.repositories;
 
 import org.example.entities.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends PagingAndSortingRepository<Task, Long> {
+    Optional<Task> findByName(String name);
 }
