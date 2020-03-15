340a276e3c5112f7611fb68e0561a29fa18f2a09
BlackAmigo <anatoliy.mir.pro@gmail.com>
2020-03-15 16:15:29 +0700

    added error message in update()

--- src/main/java/org/example/controller/TaskController.java
+++ src/main/java/org/example/controller/TaskController.java
@@ -2,13 +2,19 @@ package org.example.controller;
 
 import org.example.entities.Task;
 import org.example.entities.TaskStatus;
-import org.example.services.ValidationResult;
 import org.example.services.TaskService;
+import org.example.services.ValidationResult;
 import org.example.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "api/tasks")
@@ -20,7 +26,10 @@ public class TaskController extends AbstractController<Task> {
     @PutMapping(value = "/{id}")
     public ResponseEntity<Object> update(@PathVariable("id") Task target, Task source) {
         if (target.getStatus() == TaskStatus.CLOSED) {
-            return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+            Map<String, String> map = new HashMap<String, String>(){{
+                put("status", "The task is " + target.getStatus() + " and cannot be modified");
+            }};
+            return new ResponseEntity<>(Utils.getResultErrors(target, map),HttpStatus.BAD_REQUEST);
         } else {
             ValidationResult<Task> validationResult = taskService.update(source, target);
             if (validationResult.getErrors().isEmpty()) {
